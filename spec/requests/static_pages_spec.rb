c667dbd97bca83b6b99b46b4e4d8bb16076ae590
Luke Wright <lukewright1997@gmail.com>
2013-11-19 23:15:32 -0600

    Add contact page as part of 3.5 exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,4 +29,12 @@ describe "StaticPages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact" do
+    
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+  end
 end
