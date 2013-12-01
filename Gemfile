446f097b0ab4b7ababc82a2ce816290953ac66bd
Luke Wright <lukewright1997@gmail.com>
2013-11-30 22:19:07 -0600

    Finish user edit, update, index/destroy actions

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby '2.0.0'
 gem 'rails', '4.0.1'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 group :development do
   # Use sqlite3 as the database for Active Record
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+before_action :signed_in_user, only: [:index, :edit, :update]
+before_action :correct_user,   only: [:edit, :update]
+before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -6,9 +14,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save 
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user 
@@ -16,10 +36,31 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
-  end
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
